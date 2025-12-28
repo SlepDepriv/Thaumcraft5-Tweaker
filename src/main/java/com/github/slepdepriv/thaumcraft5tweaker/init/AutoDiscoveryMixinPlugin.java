6a5db7448c7c19704529a6798124b1ffc2973131
GitHub Action <action@github.com>
2025-12-28 00:39:02 +0000

    Initialize template repository proper
    Set package name to com.github.slepdepriv.thaumcraft5tweaker
    Set project name to Thaumcraft5-Tweaker
    Set modid to thaumcrafttweaker

--- src/main/java/com/github/slepdepriv/thaumcraft5tweaker/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/slepdepriv/thaumcraft5tweaker/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.slepdepriv.thaumcraft5tweaker.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/slepdepriv/thaumcraft5tweaker/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/slepdepriv/thaumcraft5tweaker/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.slepdepriv.thaumcraft5tweaker.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
